2dc7c923994de98ec986fcf3675ef91d64dfd0d5
Rasser <junbeomkwak@gmail.com>
2022-01-14 16:19:45 +0900

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,1 @@
+geth --networkid 42 --datadir "chain-data" --nodiscover --port 30304 --rpc --rpcport "7545" --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcapi "eth, net, web3, miner, debug, personal, rpc" console
